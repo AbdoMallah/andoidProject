f9e7a0fca6aee52ff2f82eebaada57527b571752
AbouAbed98 <55826317+AbouAbed98@users.noreply.github.com>
2021-02-09 20:36:43 +0100

    Done With Sign In Design and created CreateAccountActivity

--- app/src/main/java/se/ju/student/andoidprojectword/MainActivity.kt
+++ app/src/main/java/se/ju/student/andoidprojectword/MainActivity.kt
@@ -1,9 +1,12 @@
 package se.ju.student.andoidproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import se.ju.student.andoidprojectword.CreateAccountActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +23,23 @@ class MainActivity : AppCompatActivity() {
     /* Login With Google Account*/
     fun googleLoginOnClick(view: View) {
         Log.d("google", "Hi From Google")
+    }
+    /* Change Language To English */
+    fun englishLanguageOnClick(view: View) {}
+
+    /* Change Language To Swedish */
+    fun swedenLanguageOnClick(view: View) {}
+    /*Open Create Account Activity*/
+    fun toCreateAccountActivityOnClick(view: View) {
+        val btn = findViewById<TextView>(R.id.new_user_text_view)
+
+        btn.setOnClickListener {
+            startActivity(
+                Intent(this, CreateAccountActivity::class.java)
+            )
+
+        }
+
+
     }
 }
