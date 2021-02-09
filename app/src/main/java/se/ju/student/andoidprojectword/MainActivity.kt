396af10a6968b99e0c768560db455a37ff312e8e
AbouAbed98 <55826317+AbouAbed98@users.noreply.github.com>
2021-02-09 04:14:42 +0100

    Done With Sign In Design

--- app/src/main/java/se/ju/student/andoidprojectword/MainActivity.kt
+++ app/src/main/java/se/ju/student/andoidprojectword/MainActivity.kt
@@ -2,6 +2,8 @@ package se.ju.student.andoidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
+    }
+    /* Login with Facebook Account*/
+    fun facebookLoginOnClick(view: View) {
+        Log.d("facebook", "Hi From Facebook")
+    }
+
+    /* Login With Google Account*/
+    fun googleLoginOnClick(view: View) {
+        Log.d("google", "Hi From Google")
     }
 }
